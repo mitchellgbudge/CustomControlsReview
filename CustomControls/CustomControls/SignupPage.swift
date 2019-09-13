8fd418367ffa1a03db9958e604f3af0b9d1ba3a9
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-12 19:21:20 -0600

    Configured email entry setup

--- CustomControls/CustomControls/SignupPage.swift
+++ CustomControls/CustomControls/SignupPage.swift
@@ -14,6 +14,8 @@ class SignupPage: UIControl {
     
     let backgroundImageView: UIImageView = UIImageView()
     let emailLabel: UILabel = UILabel()
+    let emailContainerView: UIView = UIView()
+    let emailTextField: UITextField = UITextField()
     
     // MARK: - Required Initializers
     
@@ -35,7 +37,34 @@ class SignupPage: UIControl {
         backgroundImageView.image = UIImage(named: "BackgroundPhoto")
         
         // Email label
+        addSubview(emailLabel)
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        emailLabel.topAnchor.constraint(equalTo: topAnchor, constant: 150).isActive = true
+        emailLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        emailLabel.text = "Enter email here:"
+        emailLabel.font = UIFont.systemFont(ofSize: 18, weight: .light)
         
+        // Email container view
+        addSubview(emailContainerView)
+        emailContainerView.translatesAutoresizingMaskIntoConstraints = false
+        emailContainerView.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor).isActive = true
+        emailContainerView.topAnchor.constraint(equalToSystemSpacingBelow: emailLabel.bottomAnchor, multiplier: 1.0).isActive = true
+        emailContainerView.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor).isActive = true
+        emailContainerView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        emailContainerView.layer.borderColor = UIColor.black.cgColor
+        emailContainerView.layer.borderWidth = 2
+        emailContainerView.layer.cornerRadius = 5
+        
+        // Email text field
+        emailContainerView.addSubview(emailTextField)
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.leadingAnchor.constraint(equalTo: emailContainerView.leadingAnchor, constant: 8).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: emailContainerView.topAnchor, constant: 8).isActive = true
+        emailTextField.trailingAnchor.constraint(equalTo: emailContainerView.trailingAnchor, constant: -8).isActive = true
+        emailTextField.bottomAnchor.constraint(equalTo: emailContainerView.bottomAnchor, constant: -8).isActive = true
+        emailTextField.placeholder = "Email"
+    
     }
     
 }
